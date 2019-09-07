2b2d0ba38828107251f29e3394a24194b98ea8d3
823984418 <823984418@qq.com>
2019-09-07 14:53:11 +0800

    编码问题说明

--- c/cExper/1/class1/content1/test.c
+++ c/cExper/1/class1/content1/test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)//int main(int argc, char *argv[])
 {
-	printf("学号:%s\n","Xb????");//int printf(char const*,...)
+	printf("学号:%s\n","Xb????");
 	printf("姓名:%s\n","???");
 	return 0;
 }
-
