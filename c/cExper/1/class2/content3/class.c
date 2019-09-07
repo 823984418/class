0b443c1211788d1b56f23a1ab43461da484dbaf5
823984418 <823984418@qq.com>
2019-09-07 15:35:58 +0800

    第二课

--- c/cExper/1/class2/content3/class.c
+++ c/cExper/1/class2/content3/class.c
@@ -2,20 +2,15 @@
 int main(void)//int main(int argc, char *argv[])
 {
     int a,b;
-
     printf("a=");
     scanf("%d",&a);
-    
     printf("b=");
     scanf("%d",&b);
-
     int i;
     i = a;
     a = b;
     b = i;
-
     printf("a=%d\n",a);
     printf("b=%d\n",b);
-    
 	return 0;
 }
--- c/cExper/1/class2/content4/class.c
+++ c/cExper/1/class2/content4/class.c
@@ -3,13 +3,10 @@ int main(void)//int main(int argc, char *argv[])
 {
     char a,b,c;
     scanf("%c%c%c",&a,&b,&c);
-
     char i;
     i = a;
     a = c;
     c = i;
-
     printf("%c%c%c\n",a,b,c);
-
 	return 0;
 }
