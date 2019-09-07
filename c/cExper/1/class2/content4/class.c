f22ddc857d3ece244512a85ac041a2ae1fc82037
823984418 <823984418@qq.com>
2019-09-07 15:38:47 +0800

    格式化

--- c/cExper/1/class2/content4/class.c
+++ c/cExper/1/class2/content4/class.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main(void)//int main(int argc, char *argv[])
+int main(void) //int main(int argc, char *argv[])
 {
-    char a,b,c;
-    scanf("%c%c%c",&a,&b,&c);
+    char a, b, c;
+    scanf("%c%c%c", &a, &b, &c);
     char i;
     i = a;
     a = c;
     c = i;
-    printf("%c%c%c\n",a,b,c);
-	return 0;
+    printf("%c%c%c\n", a, b, c);
+    return 0;
 }
